5a0adcec857846df6cfeb73f5947717be51d0013
desaishivam26 <kane.desai@gmail.com>
2016-01-05 15:13:47 +0530

    drivers: state_notifier: sync with latest from neobuddy
    - Get rid of fb_notifiers
    - Add enable toggle and disable by default
    
    Signed-off-by: Shivam Desai <desaishivam26@yahoo.in>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1040,8 +1040,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
@@ -1055,8 +1054,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		rc = mdss_dsi_off(pdata);
 		break;
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 #endif
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)

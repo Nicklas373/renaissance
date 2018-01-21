a4d795c9ee9759e1a57cac7858f052763a4e6417
Matsuura <herlambangdicky5@gmail.com>
2018-01-21 21:05:10 +0700

    NOATIME and NODIRATIME by default
    Signed-off-by: Matsuura <herlambangdicky5@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2193,8 +2193,8 @@ long do_mount(char *dev_name, char *dir_name, char *type_page,
 		goto dput_out;
 
 	/* Default to relatime unless overriden */
-	if (!(flags & MS_RELATIME))
-		mnt_flags |= MNT_NOATIME;
+	//if (!(flags & MS_NOATIME))
+		//mnt_flags |= MNT_RELATIME;
 
 	/* Default to noatime/nodiratime unless overriden */
 	if (!(flags & MS_RELATIME))
@@ -2211,9 +2211,9 @@ long do_mount(char *dev_name, char *dir_name, char *type_page,
 		mnt_flags |= MNT_NODEV;
 	if (flags & MS_NOEXEC)
 		mnt_flags |= MNT_NOEXEC;
-	if (flags & MS_NOATIME)
+	//if (flags & MS_NOATIME)
 		mnt_flags |= MNT_NOATIME;
-	if (flags & MS_NODIRATIME)
+	//if (flags & MS_NODIRATIME)
 		mnt_flags |= MNT_NODIRATIME;
 	if (flags & MS_STRICTATIME)
 		mnt_flags &= ~(MNT_RELATIME | MNT_NOATIME);

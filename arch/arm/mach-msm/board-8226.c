a1391afe676b9062191739f23761f78796f6358e
Matsuura <herlambangdicky5@gmail.com>
2018-01-23 09:19:03 +0700

    Intelli Thermal fix
    Signed-off-by: Matsuura <herlambangdicky5@gmail.com>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -145,7 +145,7 @@ void __init msm8226_add_drivers(void)
 	fan53555_regulator_init();
 	cpr_regulator_init();
 	tsens_tm_init_driver();
-#ifndef CONFIG_INTELLI_THERMAL
+#ifdef CONFIG_INTELLI_THERMAL
 	msm_thermal_init();
 #else
 	msm_thermal_device_init(NULL);

d0723a2364d30341b10278ce0a251f01f9634493
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-02-19 12:02:10 +0700

    Revert "Added State Notifier Driver"
    This reverts commit 2410bc25617e513e642178c8a33a6bacd61b4b56.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -22,10 +22,6 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#endif
-
 #include "mdss.h"
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
@@ -1034,10 +1030,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
-#ifdef CONFIG_STATE_NOTIFIER
-			if (!use_fb_notifier)
-					state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
-#endif				
 		break;
 	case MDSS_EVENT_PANEL_ON:
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
@@ -1053,10 +1045,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
-#ifdef CONFIG_STATE_NOTIFIER
-			if (!use_fb_notifier)
-					state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
-#endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)

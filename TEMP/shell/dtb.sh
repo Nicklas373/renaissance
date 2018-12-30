82593a52ab1c0987e1d67f7eb5e333cf99855170
Mifune Hana <herlambangdicky5@gmail.com>
2018-12-30 23:56:18 +0700

    Yume: Initial Kernel Builder
    Project Yume | Codename: Minori (2018)
    
    A custom kernel for flamingo, based on linux 3.4.113
    Signed-off-by: Mifune Hana <herlambangdicky5@gmail.com>

---
+++ TEMP/shell/dtb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright 2018, Dicky Herlambang "Nicklas373" <herlambangdicky5@gmail.com>
+#
+# Yume Kernel Builder Script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+kernel_source="$HOME/kernel"
+cd $kernel_source/TEMP/dtbtool
+./dtbtool -s 2048 -o $kernel_source/arch/arm/boot/dt.img -p $kernel_source/scripts/dtc/ $kernel_source/arch/arm/boot/
+cd $kernel_source
+cp arch/arm/boot/dt.img TEMP/modules/dt.img
+rm arch/arm/boot/dt.img
---
+++ TEMP/shell/hyper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright 2018, Dicky Herlambang "Nicklas373" <herlambangdicky5@gmail.com>
+#
+# Yume Kernel Builder Script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+CROSS_COMPILE_HYPER="$HOME/arm-linux-androideabi-4.9/bin"
+kernel_zImage="$HOME/kernel/arch/arm/boot"
+kernel_source="$HOME/kernel"
+kernel_zip="TEMP/Pre-built_ZIP/ZIP"
+modules="$HOME/kernel/TEMP/modules"
+zImage="$HOME/kernel/TEMP/modules/zImage"
+export ARCH=arm
+export CROSS_COMPILE=$CROSS_COMPILE_HYPER/arm-linux-androideabi-
+make ARCH=arm yume_flamingo_defconfig
+make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE_HYPER/arm-linux-androideabi- -j$(nproc --all) -> minori.log
+cd $kernel_source
+cp $kernel_zImage/zImage $modules

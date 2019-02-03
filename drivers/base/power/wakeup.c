4dbdc9e4763437951345dcba35d38a827327156b
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-02-04 05:57:34 +0700

    Revert "wakeup: remove double bluesleep wakelock"
    This reverts commit f5d940101ae08c06ef3c7304930dae36de8a5a5c.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -31,6 +31,8 @@ static bool enable_bluesleep_ws = true;
 module_param(enable_bluesleep_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
+static bool enable_bluesleep_ws = true;
+module_param(enable_bluesleep_ws, bool, 0644);
 
 #include "power.h"
 

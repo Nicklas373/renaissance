2c87e958577c2ec8a29511372af5e53c336129d0
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-02-04 05:57:45 +0700

    Revert "wakeup: add toggle for bluesleep wakelock"
    This reverts commit c40d165c2b94220360a9e03714b74690185a527f.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -31,8 +31,6 @@ static bool enable_bluesleep_ws = true;
 module_param(enable_bluesleep_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
-static bool enable_bluesleep_ws = true;
-module_param(enable_bluesleep_ws, bool, 0644);
 
 #include "power.h"
 

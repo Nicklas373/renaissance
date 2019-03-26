69c8192070848399a1b6577ef88d10c1e40ffb47
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-03-27 03:32:41 +0700

    Revert "patch for some new governors support"
    This reverts commit c33963ff75757f6907cae31c0ff62a2f7c034c31.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -34,6 +34,7 @@
 #include <linux/completion.h>
 #include <linux/mutex.h>
 #include <linux/syscore_ops.h>
+
 #include <trace/events/power.h>
 
 #ifdef CONFIG_MSM_LIMITER
@@ -193,6 +194,15 @@ u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
 	return idle_time;
 }
 EXPORT_SYMBOL_GPL(get_cpu_idle_time);
+
+struct kobject *get_governor_parent_kobj(struct cpufreq_policy *policy)
+{
+	if (have_governor_per_policy())
+		return &policy->kobj;
+	else
+		return cpufreq_global_kobject;
+}
+EXPORT_SYMBOL_GPL(get_governor_parent_kobj);
 
 static struct cpufreq_policy *__cpufreq_cpu_get(unsigned int cpu, int sysfs)
 {
